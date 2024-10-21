1e128c3a92e07ab23f89fe0bdd1181960230e873
ShakhovDaniil <shakhov.daniil.konstantinovich@gmail.com>
2024-10-22 03:44:24 +0500

    Task7

--- Tyuiu.ShakhovDK.Sprint2.Task7.V6.Lib/DataService.cs
+++ Tyuiu.ShakhovDK.Sprint2.Task7.V6.Lib/DataService.cs
@@ -6,7 +6,7 @@ namespace Tyuiu.ShakhovDK.Sprint2.Task7.V6.Lib
         public bool CheckDotInShadedArea(double x, double y)
         {
             bool res;
-            if ((Math.Pow(x, 2) + Math.Pow(y, 2) <= 4) && ((x > -2) && (x < -1) && (y > 1) && (y < 2)) || ((x > 1) && (x < 2) && (y > 1) && (y < 2)))
+            if ((Math.Pow(x, 2) + Math.Pow(y, 2) <= 4) && ((x >= -2) && (x <= -1) && (y >= 1) && (y <= 2)) || ((x >= 1) && (x <= 2) && (y >= 1) && (y <= 2)))
             {
                 res = true;
             }
